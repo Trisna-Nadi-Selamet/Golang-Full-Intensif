866225041939bc0165c4797a3220e058361ae86d
Dies Nadi <nadi.selamet@gmail.com>
2022-06-01 20:51:36 +0700

    Golang

---
+++ Konstanta.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//jenis variabel konstanta yang nilainya tidak dapat berubah, inisialisasi nilai nya hanya bisa satu kali yaitu diawal
+    const x = 30
+
+	fmt.Printf("%d",x)
+}
