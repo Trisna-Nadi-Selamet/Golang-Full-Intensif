8a0604e3a95d0cc9a990016f0fd911dd8cbf173d
Dies Nadi <nadi.selamet@gmail.com>
2022-06-01 14:10:44 +0700

    Komentar

---
+++ Komentar.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+
+func main(){
+
+	fmt.Println("Hai Komentar Inline")//ini komentar inline : Out Hai Komentar Inline
+    fmt.Println("Hai Komentar MultyLine")
+	/*
+	ini komentar multiLine : Hai Komentar Multiline
+	*/
+
+}
